e07e306602c11528887528dffe6e123390e9f35a
tateshi <yarumaya0822@gmail.com>
2023-07-21 21:44:50 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Simple App', //アプリのタイトル
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Live!人工知能'),
+        ),
+        body: const Center(
+          child: Text('Hello Widgets!'),
+        ),
+      ),
+    );
+  }
+}
